07a71a1008bed38016a02f2825d82e0084a98031
SHIVAM JINDAL <shivam.jindal@joshtechnologygroup.com>
2020-06-20 13:10:15 +0530

    Add sample tests

--- nsestock/tests.py
+++ nsestock/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class SampleTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_functionality(self):
+        self.assertEqual(1+1, 2)
